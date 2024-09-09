e6c6e2a817e5cf7ead58875cb84f4ae0d0a92cb0
Cahlil Tillett <2021154337@ub.edu.bz>
2024-09-09 12:52:52 -0600

    hard coded json

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,7 +6,13 @@ import (
 )
 
 func (a *appDependencies) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Status: available")
-	fmt.Fprintf(w, "Env: %s\n", a.config.env)
-	fmt.Fprintf(w, "Version: %s\n", appVersion)
+	jsResponse := `{"status": "available", "environment": %q, "version": %q}`
+
+	jsResponse = fmt.Sprintf(jsResponse, a.config.env, appVersion)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(jsResponse))
+	// fmt.Fprintln(w, "Status: available")
+	// fmt.Fprintf(w, "Env: %s\n", a.config.env)
+	// fmt.Fprintf(w, "Version: %s\n", appVersion)
 }
