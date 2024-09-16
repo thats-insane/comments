cf339e3433880d182c137ba6ce182a0271a31553
Cahlil Tillett <2021154337@ub.edu.bz>
2024-09-16 12:53:17 -0600

    json

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,17 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (a *appDependencies) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	jsResponse := `{"status": "available", "environment": %q, "version": %q}`
+	data := map[string]string{
+		"status":      "available",
+		"environment": a.config.env,
+		"version":     appVersion,
+	}
 
-	jsResponse = fmt.Sprintf(jsResponse, a.config.env, appVersion)
+	jsResponse, err := json.Marshal(data)
+	if err != nil {
+		a.logger.Error(err.Error())
+		http.Error(w, "The server encountered an issue and was not able to process your request", http.StatusInternalServerError)
+		return
+	}
+
+	jsResponse = append(jsResponse, '\n')
+	// jsResponse := `{"status": "available", "environment": %q, "version": %q}`
+
+	// jsResponse = fmt.Sprintf(jsResponse, a.config.env, appVersion)
 
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(jsResponse))
+	w.Write(jsResponse)
 	// fmt.Fprintln(w, "Status: available")
 	// fmt.Fprintf(w, "Env: %s\n", a.config.env)
 	// fmt.Fprintf(w, "Version: %s\n", appVersion)
