87a99967a1c412427a2d361dea9a00d91991249b
Cahlil Tillett <2021154337@ub.edu.bz>
2024-10-02 13:14:58 -0600

    router

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -35,12 +35,9 @@ func main() {
 		logger: logger,
 	}
 
-	router := http.NewServeMux()
-	router.HandleFunc("/v1/healthcheck", appInstance.healthCheckHandler)
-
 	apiServer := &http.Server{
 		Addr:         fmt.Sprintf(":%d", settings.port),
-		Handler:      router,
+		Handler:      appInstance.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
---
+++ cmd/api/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (a *appDependencies) routes() http.Handler {
+	router := httprouter.New()
+
+	router.NotFound = http.HandlerFunc(a.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(a.notAllowedResponse)
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", a.healthCheckHandler)
+	return router
+}
