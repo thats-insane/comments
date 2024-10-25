6f0a2add71d93319e65f7c79097abada14b3b03e
Cahlil Tillett <2021154337@ub.edu.bz>
2024-10-25 13:56:12 -0600

    commit

---
+++ internal/validator/validator.go
@@ -0,0 +1,15 @@
+package validator
+
+type Validator struct {
+	Errors map[string]string
+}
+
+func New() *Validator {
+	return &Validator{
+		Errors: make(map[string]string),
+	}
+}
+
+func (v *Validator) IsEmpty() bool {
+	return len()
+}
